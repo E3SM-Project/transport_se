875243d2ffe187bfc0759049e7aa6651c035c25f
David M. Hall <halldm2000@gmail.com>
2015-05-22 14:07:47 -0700

    add configure script and run_ne test scripts.

---
+++ configure.sh
@@ -0,0 +1,103 @@
+#!/bin/csh
+#
+#  To configure and build the transport_se tracer mini-app:
+#
+#  1) Copy this script to your build directory
+#  2) Create a cmake machine file specific to your platform, if necessary
+#  2) Edit the user-defined path variables and parameters below
+#  3) Execute this script
+#
+#  note: this script is also used by test/run_tracer_tests.sh
+#_______________________________________________________________________
+
+echo "Please edit this file to specify user defined parameters"; exit # delete this line
+
+# USER-DEFINED PARAMETERS:
+
+module load cmake netcdf hdf5 ncl                 # load required modules
+
+set REPO=$HOME/CODE/transport_se                  # transport_se repository
+set WORK=$cwd                                     # work directory for building and running
+set MACH=$REPO/cmake/machineFiles/edison.cmake    # machine specific cmake file
+
+set QSIZE=4                                       # max number of tracers
+set NLEV=64                                       # number of vertical levels
+
+set RUN_COMMAND="aprun -n"                        # command to launch parallel executable
+#set RUN_COMMAND="mpirun -np"
+
+echo "REPO=$REPO"
+echo "WORK=$WORK"
+
+#_______________________________________________________________________
+# check for some common errors
+@ err=0
+
+if ($cwd == $REPO) then
+  echo "please copy this script to your build directory"
+  @ err=$err + 1
+endif
+
+if (! -f $MACH  ) then
+  echo "machine file not found: $MACH"
+  @ err=$err + 1
+endif
+
+if(`where ncl` == "") then
+  echo "ncl command not found"
+  @ err=$err + 1
+endif
+
+if($err > 0) exit
+
+#_______________________________________________________________________
+# script derived parameters
+
+set BLD_DIR   = $WORK/bld                         # build directory
+set RUN_DIR   = $WORK/run                         # run directory
+set EXE       = $BLD_DIR/src/preqx/preqx          # location of executable
+
+#_______________________________________________________________________
+# configure
+#
+#   for a debug configuration add these flags:
+#     -DCMAKE_BUILD_TYPE=Debug \
+#     -DCMAKE_Fortran_FLAGS_DEBUG="-g -O0 -fbounds-check" \
+#
+#   for native-grid (non-interpolated) output use:
+#     -DPREQX_USE_PIO=TRUE \
+#
+
+mkdir -p $BLD_DIR
+mkdir -p $RUN_DIR
+cd $BLD_DIR
+
+if ( -f $EXE  ) then
+  echo "found $EXE executable. assuming configuration was already successful"
+
+else
+  echo "running CMAKE to configure the model"
+  rm -rf CMakeFiles CMakeCache.txt src
+
+  cmake -C $MACH                \
+   -DQSIZE_D=$QSIZE             \
+   -DPREQX_PLEV=$NLEV           \
+   -DPREQX_NP=4                 \
+   -DPREQX_USE_PIO=FALSE        \
+   -DBUILD_HOMME_SWDGX=FALSE    \
+   -DBUILD_HOMME_SWEQX=FALSE    \
+   -DBUILD_HOMME_PRIMDGX=FALSE  \
+   $REPO
+
+  make -j8 clean
+endif
+
+#_______________________________________________________________________
+# build
+#
+
+echo "compiling the transport_se executable"
+make -j8 preqx
+
+exit
+
---
+++ test/run_ne30_tests.sh
@@ -0,0 +1,93 @@
+#!/bin/tcsh
+#
+#PBS -l walltime=0:05:00
+#PBS -l mppwidth=256
+#PBS -j oe
+#PBS -o out_ne30_$PBS_JOBID
+#PBS -e err_ne30_$PBS_JOBID
+
+#_______________________________________________________________________
+#
+# This file executes a set of DCMIP test to verify tracer perfrormance
+#
+#  1) Edit the path to the configuration script, if necessary
+#  2) Submit this script to the queue or execute it an interactive session
+#_______________________________________________________________________
+
+set NE       = 30         # number of elements per cube-edge
+set NCPU     = 256        # number of CPUs to use
+set NTHREADS = 1          # number of openMP threads
+set TSTEP    = 300        # timestep size
+set NU       = 1e15       # hyperviscosity coefficient
+set CONFIGURE_DIR = ../../
+
+#_______________________________________________________________________
+# get path variables from configure script:
+
+if ($?PBS_O_WORKDIR) then
+  cd $PBS_O_WORKDIR
+endif
+
+cd ${CONFIGURE_DIR}; source configure.sh
+
+set TEST1_DIR = $BLD_DIR/test/dcmip1-1  # test case directory
+set TEST2_DIR = $BLD_DIR/test/dcmip1-2  # test case directory
+set VCOORD    = $REPO/test/vcoord       # location of vertical coordinate files
+
+#_______________________________________________________________________
+# create directory for simulation output
+
+mkdir -p $RUN_DIR/movies
+cd $RUN_DIR
+cp -a $VCOORD vcoord
+
+#_______________________________________________________________________
+# create namelist for DCMIP test 1-1
+cd $TEST1_DIR
+sed s/NE.\*/$NE/ dcmip1-1.nl          |\
+sed s/TIME_STEP.\*/$TSTEP/            |\
+sed s/qsize.\*/qsize=$QSIZE/          |\
+sed s/NThreads.\*/NThreads=$NTHREADS/ |\
+sed s/nu_q.\*/nu_q=$NU/  >  $RUN_DIR/dcmip1-1_NE30.nl
+
+#_______________________________________________________________________
+# create namelist for DCMIP test 1-2
+cd $TEST2_DIR
+sed s/NE.\*/$NE/ dcmip1-2.nl          |\
+sed s/TIME_STEP.\*/$TSTEP/            |\
+sed s/qsize.\*/qsize=$QSIZE/          |\
+sed s/NThreads.\*/NThreads=$NTHREADS/ |\
+sed s/nu_q.\*/nu_q=$NU/  >  $RUN_DIR/dcmip1-2_NE30.nl
+
+#_______________________________________________________________________
+# launch the executable
+
+cd $RUN_DIR
+setenv OMP_NUM_THREADS $NTHREADS
+date
+
+# run the shorter dcmip test 1-2 first
+echo "running dcmip test 1-2"
+${RUN_COMMAND} $NCPU $EXE < dcmip1-2_NE30.nl
+if($status) exit
+
+mv HommeTime_stats HommeTime_stats_DCMIP1-2_NE30
+date
+
+# run longer dcmip test 1-1
+echo "running dcmip test 1-1"
+${RUN_COMMAND}  $NCPU $EXE < dcmip1-1_NE30.nl
+if($status) exit
+
+mv HommeTime_stats HommeTime_stats_DCMIP1-1_NE30
+date
+
+# post-processes the output
+echo "post-processing the data"
+date
+
+exit
+
+
+
+
