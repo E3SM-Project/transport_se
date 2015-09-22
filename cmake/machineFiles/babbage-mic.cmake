d6b827bf91e28f87c624038dacd3a0ed8cc3d2e6
Azamat Mametjanov <azamat@mcs.anl.gov>
2015-09-22 16:51:34 -0700

    Updates for Babbage-mic machine file
    HDF5 and NetCDF libraries needed to be built in native/mic mode.
    This updates the machine file with proper paths.

--- cmake/machineFiles/babbage-mic.cmake
+++ cmake/machineFiles/babbage-mic.cmake
@@ -1,9 +1,10 @@
 # CMake initial cache file for babbage host 
-SET (CMAKE_Fortran_COMPILER mpiifort CACHE FILEPATH "")
-SET (CMAKE_C_COMPILER mpiicc CACHE FILEPATH "")
-SET (CMAKE_CXX_COMPILER mpiicpc CACHE FILEPATH "")
-SET (NETCDF_DIR $ENV{NETCDF_DIR} CACHE FILEPATH "")
+SET (CMAKE_Fortran_COMPILER /global/babbage/nsg/opt/intel/impi/5.0.2.044/intel64/bin/mpiifort CACHE FILEPATH "")
+SET (CMAKE_C_COMPILER /global/babbage/nsg/opt/intel/impi/5.0.2.044/intel64/bin/mpiicc CACHE FILEPATH "")
+SET (CMAKE_CXX_COMPILER /global/babbage/nsg/opt/intel/impi/5.0.2.044/intel64/bin/mpiicpc CACHE FILEPATH "")
+
+SET (HDF5_DIR /global/homes/a/azamat/babbage/soft/hdf5/1.8.14 CACHE FILEPATH "")
+SET (NETCDF_DIR /global/homes/a/azamat/babbage/soft/netcdf/4.4.2 CACHE FILEPATH "")
 SET (WITH_PNETCDF FALSE CACHE BOOL "")
-SET (HDF5_DIR $ENV{HDF5_DIR} CACHE FILEPATH "")
 
 SET (ENABLE_INTEL_PHI TRUE CACHE BOOL "")
