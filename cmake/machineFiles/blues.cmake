63a5e375ad6a8866bbf57fd408d9030b5c17b38c
Azamat Mametjanov <azamat@mcs.anl.gov>
2016-09-30 18:00:56 -0500

    Machine file for ANL Blues

---
+++ cmake/machineFiles/blues.cmake
@@ -0,0 +1,10 @@
+# CMake initial cache file for Blues
+SET (CMAKE_Fortran_COMPILER mpif90 CACHE FILEPATH "")
+SET (CMAKE_C_COMPILER mpicc CACHE FILEPATH "")
+SET (CMAKE_CXX_COMPILER mpicxx CACHE FILEPATH "")
+
+SET (HDF5_DIR /soft/climate/hdf5/1.8.16-parallel/intel-15.0.1/mvapich2-2.2a-intel-15.0 CACHE FILEPATH "")
+SET (NETCDF_DIR /soft/climate/netcdf/4.3.3.1c-4.2cxx-4.4.2f-serial/intel-15.0.1 CACHE FILEPATH "")
+SET (PNETCDF_DIR /soft/climate/pnetcdf/1.6.1/intel-15.0.1/mvapich2-2.2a-intel-15.0 CACHE FILEPATH "")
+SET (SZIP_DIR /soft/climate/szip/2.1/intel-15.0.1 CACHE FILEPATH "")
+
