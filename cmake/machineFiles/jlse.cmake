adf83ae540440387031c0b8aa055d646adb02994
Azamat Mametjanov <azamat@mcs.anl.gov>
2020-08-07 22:41:48 +0000

    Add JLSE updates

---
+++ cmake/machineFiles/jlse.cmake
@@ -0,0 +1,20 @@
+SET (CMAKE_Fortran_COMPILER mpif90 CACHE FILEPATH "")
+SET (CMAKE_C_COMPILER mpicc CACHE FILEPATH "")
+SET (CMAKE_CXX_COMPILER mpicxx CACHE FILEPATH "")
+
+SET (NETCDF_DIR /gpfs/jlse-fs0/projects/climate/soft/netcdf/4.4.1c-4.2cxx-4.4.4f/intel19 CACHE FILEPATH "")
+SET (PNETCDF_DIR /gpfs/jlse-fs0/projects/climate/soft/pnetcdf/1.12.1/intel19 CACHE FILEPATH "")
+
+EXECUTE_PROCESS(COMMAND ${NETCDF_DIR}/bin/nf-config --flibs
+  RESULT_VARIABLE NFCONFIG_RESULT
+  OUTPUT_VARIABLE NFCONFIG_OUTPUT
+  ERROR_VARIABLE  NFCONFIG_ERROR
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)  
+IF (${NFCONFIG_ERROR})
+  MESSAGE(WARNING "${NETCDF_DIR}/bin/nf-config --flibs produced an error. Default linking will be used.")
+ELSE ()  
+  SET (ADD_LINKER_FLAGS " ${NFCONFIG_OUTPUT} " CACHE STRING "")
+  MESSAGE(STATUS "Added ${NFCONFIG_OUTPUT} to link flags: ${ADD_LINKER_FLAGS}")
+ENDIF ()
+
